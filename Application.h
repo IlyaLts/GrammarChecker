4980498bdff7a62974663d6e98b28e53bfb0d85b
Ilya Lyakhovets <ilyalyakhovets@gmail.com>
2025-05-25 19:33:27 +0700

    Update version

--- Application.h
+++ Application.h
@@ -25,7 +25,7 @@
 
 #define gcApp (static_cast<Application *>(QCoreApplication::instance()))
 
-#define GRAMMAR_CHECKER_VERSION     "1.2"
+#define GRAMMAR_CHECKER_VERSION     "1.2.1"
 #define SETTINGS_FILENAME           "Settings.ini"
 
 struct Language
